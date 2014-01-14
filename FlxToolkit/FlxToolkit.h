753baab3455964dfe00fd65a7ced478ccf1920e5
Aaron Hayman <aaron@flexile.co>
2014-01-14 12:31:38 -0500

    Initial Commit

---
+++ FlxToolkit/FlxToolkit.h
@@ -0,0 +1,13 @@
+//
+//  FlxToolkit.h
+//  FlxToolkit
+//
+//  Created by Aaron Hayman on 1/14/14.
+//  Copyright (c) 2014 Aaron Hayman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FlxToolkit : NSObject
+
+@end
---
+++ FlxToolkit/FlxToolkit.m
@@ -0,0 +1,13 @@
+//
+//  FlxToolkit.m
+//  FlxToolkit
+//
+//  Created by Aaron Hayman on 1/14/14.
+//  Copyright (c) 2014 Aaron Hayman. All rights reserved.
+//
+
+#import "FlxToolkit.h"
+
+@implementation FlxToolkit
+
+@end
