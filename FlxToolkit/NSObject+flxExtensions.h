4fbbd1f27897c6d751bf021f385c58e67eb533da
Aaron Hayman <aaron@flexile.co>
2014-03-11 19:21:12 -0400

    - Added methods to NSObject to perform selectors only once. This allows you to call the method multiple times and the selector will only be performed once.

--- FlxToolkit/NSObject+flxExtensions.h
+++ FlxToolkit/NSObject+flxExtensions.h
@@ -17,4 +17,18 @@
  This will compare two objects taking in to account nil values and class types. Class type comparison is done using 'isKindOfClass' on 'object2'.  If either object responds to 'equalTo:', this will be used.  Otherwise, direct pointer comparison will be used.
  **/
 + (BOOL) object:(id)object1 equalTo:(id)object2;
+/**
+ *  This will perform the specified selector on the main thread only once.  Attempts to perform the selector additional times before it has been performed will be ignored.  After the selector has been performed, you can then perform it again if you need to.  Use this method to prevent the selector from being fired multiple times.
+ *
+ *  @param selector The selector you want to perform
+ *  @param object   The object you want to send to the selector
+ *  @param delay    The delay you want until the selector is performed.
+ */
+- (void) performSelectorOnceOnMain:(SEL)selector withObject:(id)object afterDelay:(NSTimeInterval)delay;
+/**
+ *  This will perform the selector after a delay of 0. This should perform the selector at the end of the run loop. Additional attempts to perform the selector before it has been performed will be ignored.  The selector is performed using `NSRunLoopCommonModes`.
+ *
+ *  @param selector The selector you want to perform.
+ */
+- (void) performSelectorOnceAfterDelay:(SEL)selector;
 @end
--- FlxToolkit/NSObject+flxExtensions.m
+++ FlxToolkit/NSObject+flxExtensions.m
@@ -9,6 +9,9 @@
 #import "NSObject+flxExtensions.h"
 #include <objc/runtime.h>
 
+#define FlxNeedToPerformKey @"FlxNeedsToPerformSelectorKey"
+#define FlxConsolidatedPerformKey @"FlxConsolidatedPerformKey"
+
 @implementation NSObject (flxExtensions)
 + (BOOL) object:(id)object1 equalTo:(id)object2{
     if (!object1 && !object2) return YES;
@@ -28,5 +31,42 @@
         objc_setAssociatedObject(self, associated_key, info, OBJC_ASSOCIATION_RETAIN);
     }
     return info;
+}
+- (void) performSelectorOnceOnMain:(SEL)selector withObject:(id)object afterDelay:(NSTimeInterval)delay{
+    NSMutableDictionary *associatedInfo = self.associatedInfo;
+    NSMutableDictionary *selectors = associatedInfo[FlxNeedToPerformKey];
+    if (!selectors){
+        selectors = [NSMutableDictionary new];
+        associatedInfo[FlxNeedToPerformKey] = selectors;
+    }
+    NSString *selectorString = NSStringFromSelector(selector);
+    id selectorObject = selectors[selectorString];
+    if (!selectorObject){
+        [self performSelector:@selector(_flxPerformSelector:) withObject:selectorString afterDelay:delay inModes:@[NSRunLoopCommonModes]];
+        selectors[selectorString] = object ? : [NSNull null];
+    }
+}
+- (void) _flxPerformSelector:(NSString *)selectorString{
+    SEL selector = NSSelectorFromString(selectorString);
+    NSMutableDictionary *selectors = self.associatedInfo[FlxNeedToPerformKey];
+    id object = selectors[selectorString];
+    selectors[selectorString] = nil;
+    [self performSelectorOnMainThread:selector withObject:(object != [NSNull null]) ? object : nil waitUntilDone:NO];
+}
+- (void) performSelectorOnceAfterDelay:(SEL)selector{
+    NSMutableArray *selectors = self.associatedInfo[FlxConsolidatedPerformKey];
+    if (!selectors){
+        selectors = [NSMutableArray new];
+        self.associatedInfo[FlxConsolidatedPerformKey] = selectors;
+        [self performSelector:@selector(_flxConsolidatedSelectorPerform) withObject:nil afterDelay:0 inModes:@[NSRunLoopCommonModes]];
+    }
+    [selectors addObject:NSStringFromSelector(selector)];
+}
+- (void) _flxConsolidatedSelectorPerform{
+    NSMutableArray *selectors = self.associatedInfo[FlxConsolidatedPerformKey];
+    self.associatedInfo[FlxConsolidatedPerformKey] = nil;
+    for (NSString *selectorString in selectors){
+        [self performSelectorOnMainThread:NSSelectorFromString(selectorString) withObject:nil waitUntilDone:NO];
+    }
 }
 @end
